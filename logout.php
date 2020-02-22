68e058521de47562ae9e2b95f076934870c08ee4
davidsampson12 <54995715+davidsampson12@users.noreply.github.com>
2020-02-22 11:34:31 -0500

    Merged wrong folder - - corrected upload

--- logout.php
+++ logout.php
@@ -1,8 +1,13 @@
-<?
+<?php
+include("config.php");
+include("lib/db.php");
+
+
 session_start();
+
 session_unset();
 session_destroy();
 
-header("Location: /");
+header("Location: /"); 
 exit();
 ?>
